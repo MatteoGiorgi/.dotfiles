f3764cc440dba2ad0538c4b457ef213712ac5360
Matteo Giorgi <matteo.giorgi@protonmail.com>
2020-12-01 20:36:30 +0100

    volume notification fixed

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -1,17 +1,20 @@
 #! /usr/bin/env bash
 
 # -> This file launches some programs at startup, it is ment to be called at the end of config.py (Qtile config file).
-# -> For Arch install use picom, for others like Ubuntu use compton (comment the unused).
-# -> As bluetooth applet I use blueman-applet, but some other distros like Ubuntu use blueberry-tray.
+# -> For Arch install use `picom -c`, for others like Ubuntu use `compton -c`.
+# -> As bluetooth applet I use `blueman-applet`, but some other distros like Ubuntu use `blueberry-tray`.
 # -> Remember to install xfce4-volumed too.
-# -> Make sure you have the right path for xfce4-notifyd (comment the wrong one for your system).
+# -> To use Music on Console from the systray, use `mocicon`.
+# -> Make sure you have the right path for xfce4-notifyd:
+#        `/usr/lib/x86_64-linux-gnu/xfce4/notifyd/xfce4-notifyd` for Arch
+#        `/usr/lib/xfce4/notifyd/xfce4-notifyd` for Ubuntu
 
-compton -c & # picom -c &
+compton -c &
 ~/.fehbg &
+xfce4-volumed &
 volumeicon &
-mocicon &
 nm-applet &
 blueman-applet &
 xfce4-power-manager &
-/usr/lib/x86_64-linux-gnu/xfce4/notifyd/xfce4-notifyd & # /usr/lib/xfce4/notifyd/xfce4-notifyd &
+/usr/lib/x86_64-linux-gnu/xfce4/notifyd/xfce4-notifyd &
 kupfer --no-splash
